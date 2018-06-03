38333f2fe77e82e6bef63b9a845e6c7a2de6b42e
周金龙 <280227603@qq.com>
2018-06-03 07:03:50 +0000

    贴子详情页制作

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -25,6 +25,7 @@ class TopicsController extends Controller
 
     public function show(Topic $topic)
     {
+
         return view('topics.show', compact('topic'));
     }
 
@@ -40,7 +41,7 @@ class TopicsController extends Controller
 		$topic->user_id = Auth::id();
 		$topic->save();
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('message', '发布成功.');
 	}
 
 	public function edit(Topic $topic)
@@ -54,7 +55,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('message', '更新成功.');
 	}
 
 	public function destroy(Topic $topic)
@@ -62,7 +63,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('message', '删除成功.');
 	}
 
     public function uploadImage(Request $request, ImageUploadHandler $uploader)

492df398c7efe9dc7d9f20d36ee7407793d9cfc2
周金龙 <280227603@qq.com>
2018-06-03 13:37:23 +0000

    用户回复功能完成

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -6,6 +6,7 @@ use App\Models\Reply;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ReplyRequest;
+use Auth;
 
 class RepliesController extends Controller
 {
@@ -14,41 +15,17 @@ class RepliesController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
+
+	public function store(ReplyRequest $request, Reply $reply)
 	{
-		$replies = Reply::paginate();
-		return view('replies.index', compact('replies'));
+        $reply->content = $request->input('content');
+        $reply->user_id = Auth::id();
+        $reply->topic_id = $request->input('topic_id');
+        $reply->save();
+
+		return redirect()->to($reply->topic->link())->with('message', '回复成功.');
 	}
 
-    public function show(Reply $reply)
-    {
-        return view('replies.show', compact('reply'));
-    }
-
-	public function create(Reply $reply)
-	{
-		return view('replies.create_and_edit', compact('reply'));
-	}
-
-	public function store(ReplyRequest $request)
-	{
-		$reply = Reply::create($request->all());
-		return redirect()->route('replies.show', $reply->id)->with('message', 'Created successfully.');
-	}
-
-	public function edit(Reply $reply)
-	{
-        $this->authorize('update', $reply);
-		return view('replies.create_and_edit', compact('reply'));
-	}
-
-	public function update(ReplyRequest $request, Reply $reply)
-	{
-		$this->authorize('update', $reply);
-		$reply->update($request->all());
-
-		return redirect()->route('replies.show', $reply->id)->with('message', 'Updated successfully.');
-	}
 
 	public function destroy(Reply $reply)
 	{

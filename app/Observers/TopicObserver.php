0cd5c5d8aa39a2a4cdc89acdd473a9e34422c030
周金龙 <280227603@qq.com>
2018-06-04 14:08:16 +0000

    删除回复

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -43,4 +43,10 @@ class TopicObserver
             dispatch(new TranslateSlug($topic));
         }
     }
+
+    public function deleted(Topic $topic){
+
+        \DB::table('replies')->where('topic_id', $topic->id)->delete();
+
+    }
 }

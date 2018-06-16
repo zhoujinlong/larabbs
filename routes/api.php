f34dc3225c16ab318da2a206bdd10303fd3d69a9
周金龙 <280227603@qq.com>
2018-06-16 09:28:47 +0000

    接口访问次数限制

--- routes/api.php
+++ routes/api.php
@@ -20,11 +20,22 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
-], function($api){
-    //发送短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
 
-    //用户注册
-    $api->post('users', 'UsersController@store')->name('api.users.store');
+    'namespace' => 'App\Http\Controllers\Api'
+
+], function($api){
+
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function($api) {
+
+        // 短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
+        // 用户注册
+        $api->post('users', 'UsersController@store')->name('api.users.store');
+
+    });
+
 });

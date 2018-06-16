644c180d3bf1b33770710f0e70b2a0809fd5a75d
周金龙 <280227603@qq.com>
2018-06-16 16:10:32 +0000

    add jwt

--- routes/api.php
+++ routes/api.php
@@ -39,6 +39,8 @@ $api->version('v1', [
         $api->post('captchas', 'CaptchasController@store')->name('api.captchas.store');
         // 第三方登录
         $api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')->name('api.socials.authorizations.store');
+        // 登录
+        $api->post('authorizations', 'AuthorizationsController@store')->name('api.authorizations.store');
 
     });
 

b156008a5e554b67af19b868280f00a95738b043
周金龙 <280227603@qq.com>
2018-06-22 13:34:49 +0000

    获取当前用户的权限列表

---
+++ app/Transformers/PermissionTransformer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+use Spatie\Permission\Models\Permission;
+
+class PermissionTransformer extends TransformerAbstract{
+
+    public function transform(Permission $permission){
+
+        return [
+            'id' => $permission->id,
+            'name' => $permission->name,
+        ];
+
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -92,6 +92,8 @@ $api->version('v1', [
             $api->get('user/notifications/stats', 'NotificationsController@stats')->name('api.user.notifications.stats');
             //标记消息已读
             $api->patch('user/read/notifications', 'NotificationsController@read')->name('api.user.notifications.read');
+            //当前登录用户权限
+            $api->get('user/permissions', 'PermissionsController@index')->name('api.user.permissions.index');
         });
     });
 

a190f2561fe6a9bc7cb28186813913b906763d52
周金龙 <280227603@qq.com>
2018-06-23 03:08:41 +0000

    推荐资源

---
+++ app/Transformers/LinkTransformer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+use App\Models\Link;
+
+class LinkTransformer extends TransformerAbstract{
+
+    public function transform(Link $link){
+
+        return [
+            'id' => $link->id,
+            'title' => $link->title,
+            'link' => $link->link,
+        ];
+
+
+    }
+}

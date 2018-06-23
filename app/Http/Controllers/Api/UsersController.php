3cf28d8f92d181e901fb3053b94afb4e7392e337
周金龙 <280227603@qq.com>
2018-06-23 03:25:17 +0000

    活跃用户

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -61,4 +61,10 @@ class UsersController extends Controller
     {
         return $this->response->item($this->user(), new UserTransformer());
     }
+
+    public function activedIndex(User $user){
+
+        return $this->response->collection($user->getActiveUsers(), new UserTransformer());
+
+    }
 }
